fe0d0d8590f07de50db9784cc46a8fe1194aab4e
zhangpeisong <zhangpeisong_ps@outlook.com>
2026-03-31 17:28:40 +0800

    qwen32b

--- micro2024/chakra-demo/demo3/run_demo3.sh
+++ micro2024/chakra-demo/demo3/run_demo3.sh
@@ -7,9 +7,9 @@ STG=${SCRIPT_DIR}/../../symbolic_tensor_graph
 
 # Run Symbolic Tensor Graph (STG) Generator
 (
-python ${STG}/main.py --output_dir ${SCRIPT_DIR}/generated/ \
+cd ${STG}
+python main.py --output_dir ${SCRIPT_DIR}/generated/ \
                --output_name workload.%d.et \
-               --comm_group_file comm_group.json \
                --dp 2 --tp 2 --pp 2 \
                --weight_sharded 0
 )
